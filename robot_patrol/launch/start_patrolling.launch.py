0dbd1154c6133ccb25e85c2ad12a2f72ce0f859a
sravindr1 <sravindr@mtu.edu>
2025-07-18 19:49:17 +0000

    Patrol node first version

---
+++ robot_patrol/launch/start_patrolling.launch.py
@@ -0,0 +1,15 @@
+from launch import LaunchDescription
+from loaunch_ros.actions import Node
+
+def generate_launch_description():
+
+    
+    
+    return LaunchDescription([
+    Node(
+    package = "robot_patrol",
+    executable = "robot_patrol_node",
+    output ="screen",
+    name = "robot_patrol_node" # name optional
+    ),
+    ])
