9ace3becab73dad3d1b2e80dfad68cb70cbd1973
sravindr1 <sravindr@mtu.edu>
2025-07-20 17:50:39 +0000

    modify CMakeLists.txt to add rviz directory to share directory, add 2nd node to launch rviz config file

--- robot_patrol/launch/start_patrolling.launch.py
+++ robot_patrol/launch/start_patrolling.launch.py
@@ -1,10 +1,19 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
+from ament_index_python.packages import get_package_share_directory
+import os
 
 def generate_launch_description():
 
+    rviz_config = os.path.join(
+        get_package_share_directory('robot_patrol'),
+        'rviz',
+        'patrol_config.rviz'
+    )
     
-    
+    print(rviz_config)
+    print(get_package_share_directory('robot_patrol'))
+
     return LaunchDescription([
     Node(
     package = "robot_patrol",
@@ -12,4 +21,13 @@ def generate_launch_description():
     output ="screen",
     name = "robot_patrol_node" # name optional
     ),
+
+  Node(
+    package = "rviz2",
+    executable = "rviz2",
+    output ="screen",
+    arguments=['-d', rviz_config],
+    name = "rviz2" # name optional
+    ),
+
     ])
